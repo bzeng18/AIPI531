af82e76084892d53db907aa3b36314fa2097716b
bzeng18 <40479816+bzeng18@users.noreply.github.com>
2023-09-28 20:09:21 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='zengboyuan',
+    author_email='',
+    description=''
+)
